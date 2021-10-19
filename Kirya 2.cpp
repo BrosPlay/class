47788610e6bf4e330acb8b9d900160b78a4d731e
BrosPlay <91685888+BrosPlay@users.noreply.github.com>
2021-10-19 16:46:07 +0300

    Add files via upload

---
+++ Kirya 2.cpp
@@ -0,0 +1,27 @@
+﻿#include<iostream>
+using namespace std;
+int main() {
+	/*
+	Boolean1◦
+. Дано целое число A. Проверить истинность высказывания: «Число A является положительным».
+	*/
+	/*
+	int A;
+	bool res;
+	cin >> A;
+	res = A > 0;
+	cout << boolalpha;
+	cout << res << endl;
+	*/
+	/*
+	Boolean2◦
+. Дано целое число A. Проверить истинность высказывания: «Число A является нечетным».
+*/
+	int A;
+	bool res;
+	cin >> A;
+	res = A % 2 != 0;
+	cout << boolalpha;
+	cout << res << endl;
+}
+
